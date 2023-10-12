94997c7f59de534f8f1713a4688d5ab57ffee300
Gilberto <josegilbertoguzmangutierrez@gmail.com>
2023-10-11 19:26:55 -0600

    actualizacion del contenido

--- example.pl
+++ example.pl
@@ -1,31 +1,54 @@
-main:-
-writef('\n** Horarios Escolares **\n\n'),
-consult('horario-kb'), % Consulta
-base-conocimientos
-mats, % Lista materias
-docs, % Lista docentes
-generar, % Genera combinaciones posibles
-hora(8). % Que opciones hay a las 8am?
-mats:-
-findall(M,mat(M),L),
-length(L,X),
-writef('%w Materias: %w\n',[X,L]).
-docs:-
-findall(P,profr(P),L),
-length(L,X),
-writef('%w Docentes: %w\n\n',[X,L]).
-generar:-
-pm(P,M),
-ph(P,H),
-writef('Docente: %w,\tMat: %w,\tHr:
-%w\n',[P,M,H]),
-fail.
-generar.
-hora(H):-
-writef('\nHora: %w\n',[H]),
-ph(P,H),
-pm(P,M),
-writef('Mat: %w,\tDocente: %w\n',[M,P]),
-fail.
-hora(_).
-:- main.
+% Programa Menu.pl
+
+imprime :-
+    write('Mi nombre es Daniel'), nl,
+    write('Universidad Autónoma de Chiapas').
+
+% Hechos sobre los alimentos y sus tiempos
+entrada(ensalada).
+entrada(sopa).
+entrada(crema).
+
+carne(res).
+carne(pollo).
+carne(puerco).
+
+pescado(mojarra).
+pescado(robalo).
+pescado(lisa).
+
+postre(fruta).
+postre(flan).
+postre(helado).
+
+% Reglas que definen el plato principal y la comida de tres tiempos
+plato_principal(X) :- carne(X).
+plato_principal(X) :- pescado(X).
+
+comida(A, B, C) :- entrada(A), plato_principal(B), postre(C).
+
+% Hechos que asignan las calorías a los alimentos
+calorias(ensalada, 30).
+calorias(sopa, 60).
+calorias(crema, 90).
+calorias(res, 200).
+calorias(puerco, 250).
+calorias(pollo, 180).
+calorias(mojarra, 100).
+calorias(robalo, 150).
+calorias(lisa, 170).
+calorias(fruta, 50).
+calorias(flan, 80).
+calorias(helado, 100).
+
+% GOALS
+valor_calorico(E, PP, P, V) :-
+    comida(E, PP, P),
+    calorias(E, X),
+    calorias(PP, Y),
+    calorias(P, Z),
+    V is X + Y + Z.
+
+menu_equilibrado(X, Y, Z, V) :-
+    valor_calorico(X, Y, Z, V),
+    V =< 280.
